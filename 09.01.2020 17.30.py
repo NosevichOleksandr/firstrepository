0621ed430c5038bb4085012b9ac0c284dd8a1edb
SASHA <bpxnastalgia@gmail.com>
2021-04-10 18:40:40 +0300

    smth

--- 09.01.2020 17.30.py
+++ 09.01.2020 17.30.py
@@ -2,5 +2,5 @@ def func(lkm):
     b = 0
     for i in str(lkm):
         b += int(i)
-    print(True if lkm > b**len(str(lkm)) else False)
+    return True if lkm > b**len(str(lkm)) else False
 print(func(123))
