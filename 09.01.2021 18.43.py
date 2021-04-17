70652118bcdac48f7b638fe2d896d587f79115d1
SASHA <bpxnastalgia@gmail.com>
2021-04-17 16:34:37 +0300

    'smth'

--- 09.01.2021 18.43.py
+++ 09.01.2021 18.43.py
@@ -1,22 +1,10 @@
-def qw():
-    file = open('0.txt' , 'r')
-    smth = ''
-    word = ''
-    answ = 0
-    for i in file:
-        if i != '!' or i != ':' or i != ';' or i != "'" or i != '"' or i != '.' or i != ',':
-            smth += i
-
-    smth += ' '
-
-    for i in smth:
-        if i != ' ':
-            word += i
-        else:
-            if len(word) % 2 == 0:
-                answ += 1
-            word = ''
-    print(smth)
-    print(answ)
-    file.close()
+a = int(input('number'))
+answ = 0
+while True:
+    if a%2 == 0:
+        a = a//2
+        answ += 1
+    else:
+        break
+print(answ)
 
